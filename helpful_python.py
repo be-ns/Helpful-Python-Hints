c00b6d481197c60a7b5fbd75616bea28c0087706
Benjamin S <benjaminsiverly@gmail.com>
2017-02-15 13:52:20 -0700

    Update helpful_python
    added 'operator' library hints

--- helpful_python.py
+++ helpful_python.py
@@ -13,3 +13,9 @@ zfill()
 # builtin function that pads leftside of string with 0 unless it is the required amount of characters
 # so if hi = 'hi'
 hi.zfill(3) would return '0hi'
+
+
+# `operator` library
+# This class turns the standard mathematical calculations you write, `+` and `-`, into functions, `sum()` and `sub()`, respectively.
+# These functions can make your life easier as you're trying to implement your program.
+# This can also allow for more complicated operations (`sin`, `tan`, `cos`, and `pi`).
