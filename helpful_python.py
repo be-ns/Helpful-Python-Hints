c5b8414794cf6cd60d0b6998bc122c7bda884bfa
Benjamin S <benjaminsiverly@gmail.com>
2017-04-07 14:37:16 -0600

    adde matplotlib help

--- helpful_python.py
+++ helpful_python.py
@@ -19,3 +19,14 @@ hi.zfill(3) would return '0hi'
 # This class turns the standard mathematical calculations you write, `+` and `-`, into functions, `sum()` and `sub()`, respectively.
 # These functions can make your life easier as you're trying to implement your program.
 # This can also allow for more complicated operations (`sin`, `tan`, `cos`, and `pi`).
+
+
+# fixing plots in matplotlib where whitespace is bad
+def fix_whitespace(ax,buff=0.01):
+    """use x and y to add well spaced margins"""
+xmin,xmax = ax.get_xlim()
+ymin,ymax = ax.get_ylim()
+xbuff = buff * (xmax - xmin)
+ybuff = buff * (ymax - ymin)
+ax.set_xlim(xmin-xbuff,xmax+xbuff)
+ax.set_ylim(ymin-ybuff,ymax+ybuff)
